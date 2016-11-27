b7627b4a7cba0e50409d462c0e62ed0676fe6180
Tuukka Laitinen <ictlaitinenoy@gmail.com>
2016-11-27 10:18:02 +0200

    Navigating to index now works and a list of band names is delivered by React

---
+++ src/main/js/app.js
@@ -0,0 +1,55 @@
+const React = require('react');
+const ReactDOM = require('react-dom')
+const client = require('./client');
+
+class App extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {bands: []};
+	}
+
+	componentDidMount() {
+		client({method: 'GET', path: '/api/bands'}).done(response => {
+			this.setState({bands: response.entity._embedded.bands});
+		});
+	}
+
+	render() {
+		return (
+			<BandList bands={this.state.bands}/>
+		)
+	}
+}
+
+class BandList extends React.Component{
+	render() {
+		var bands = this.props.bands.map(band =>
+			<Band key={band._links.self.href} band={band}/>
+		);
+		return (
+			<table>
+				<tbody>
+					<tr>
+						<th>Name</th>
+					</tr>
+					{bands}
+				</tbody>
+			</table>
+		)
+	}
+}
+
+class Band extends React.Component{
+	render() {
+		return (
+			<tr>
+				<td>{this.props.band.name}</td>
+			</tr>
+		)
+	}
+}
+
+ReactDOM.render(
+		<App />,
+		document.getElementById('react'))
