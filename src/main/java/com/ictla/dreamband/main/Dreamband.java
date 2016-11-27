6b6476bc117494e6f4793df0a85e708ad8760afb
Tuukka Laitinen <ictlaitinenoy@gmail.com>
2016-11-27 06:24:14 +0200

    Added the missing ComponentScan annotion which is needed at the root of the package hierarchy

--- src/main/java/com/ictla/dreamband/main/Dreamband.java
+++ src/main/java/com/ictla/dreamband/main/Dreamband.java
@@ -3,9 +3,11 @@ package com.ictla.dreamband.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan("com.ictla")
 @EntityScan(basePackages = "com.ictla.dreamband.domain")
 @EnableJpaRepositories(basePackages = "com.ictla.dreamband.repository")
 public class Dreamband {
